11e1915d4f608c51eaa1b9689f5d8e8b98ad968c
bodhiintellect <nath.syam.1986@gmail.com>
2025-12-23 03:20:23 +0530

    Initial commit

---
+++ sw.js
@@ -0,0 +1,69 @@
+// sw.js - Service Worker
+const CACHE_NAME = 'securevideo-v7';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/favicon-32x32.png',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  console.log('Service Worker: Installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Service Worker: Caching assets');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+      .then(() => {
+        console.log('Service Worker: Caching complete, activating...');
+        return self.skipWaiting();
+      })
+      .catch(error => {
+        console.error('Service Worker: Caching failed', error);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker: Activating...');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Service Worker: Deleting old cache', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+      console.log('Service Worker: Activated and claimed clients.');
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Service Worker: Fetching', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          console.log('Service Worker: Found in cache', event.request.url);
+          return response;
+        }
+        console.log('Service Worker: Not in cache, fetching from network', event.request.url);
+        return fetch(event.request);
+      })
+  );
+});
